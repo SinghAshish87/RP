db71a095a594093486ba06c5f06bb8182fa2ffd1
Ashish singh <rockyrock8961@gmail.com>
2025-11-24 22:39:08 +0530

    Add files via upload

---
+++ new potfolio react.js/rp2/src/components/Pages/Projects.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <div className="projects">
+      <h1>My Projects</h1>
+      <p>Here are some of the projects I've worked on:</p>
+      <ul>
+        <li>Project 1</li>
+        <li>Project 2</li>
+        <li>Project 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Projects;
