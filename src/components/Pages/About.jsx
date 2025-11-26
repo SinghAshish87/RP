6823aed73330794f1e0a3d4f59ed516ae0b7afd1
gautamkaran <karankumargautam.dev@gmail.com>
2025-11-26 10:21:58 +0530

    fix folder structure

---
+++ src/components/Pages/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about-us">
+      <h1>About Me</h1>
+      <p>Hi, I’m [Your Name]. I’m a passionate web developer specializing in React.</p>
+    </div>
+  );
+}
+
+export default About;
