79770fd69bd39dff46c93ac2d7e85e248e10de2e
gautamkaran <karankumargautam.dev@gmail.com>
2025-11-26 11:05:29 +0530

    fix navbar component display

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import About from "./components/Pages/About";
 import Projects from "./components/Pages/Projects";
 import Contact from "./components/Pages/Contact";
 import "./App.css"; // Assuming you have some basic 
+import Navbar from "./components/Pages/Navbar";
 
 
 function App() {          
   return (
     <BrowserRouter>
+    <Navbar/>
     <Routes>
           <Route path="/"  element={<Home/>} />
           <Route path="/about" element={<About/>} />
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,10 +1,7 @@
-import Navbar from "./Navbar";
-
 
 function Home() {
   return (
     <div className="home">
-      <Navbar/>
       <h1>Welcome to My Portfolio</h1>
       <p>This is the home page. Check out my work!</p>
     </div>
